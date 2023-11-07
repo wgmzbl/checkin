0fe0f33b536b9a4f00af1ca6d526b97de095c1f9
wgmzbl <64002380+wgmzbl@users.noreply.github.com>
2023-11-08 00:25:46 +0800

    Update glados.space

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const glados = async () => {
       'referer': 'https://glados.rocks/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
+    const checkin = await fetch('https://glados.space/api/user/checkin', {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
       body: '{"token":"glados.one"}',
     }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
+    const status = await fetch('https://glados.space/api/user/status', {
       method: 'GET',
       headers,
     }).then((r) => r.json())
